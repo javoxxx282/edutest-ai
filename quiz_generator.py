dff24fb3d8b116f0e18656abd395c3f256f9e4dd
javoxxx282 <jokha282@gmail.com>
2026-05-28 15:20:21 +0300

    Update quiz_generator.py

--- quiz_generator.py
+++ quiz_generator.py
@@ -4,7 +4,7 @@ import os
 from google import genai
 from google.genai import types
 
-client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY", ""))
 
 QUIZ_PROMPT = """
 Siz o'zbek tilida test savollari tayyorlaydigan mutaxasssissiz.
